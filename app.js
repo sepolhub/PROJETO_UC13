918d0e732b78802ff72164f5a48514bae96c8846
Nathanlc <nathanlcastro81@gmail.com>
2025-05-28 22:07:50 -0300

    Minha primeira rota em node js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express = require ('express');
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.write("Hello World");
+    res.end();
+});
+
+app.listen(8080);
+
