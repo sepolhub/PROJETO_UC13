b73965990f596ab047e7b1f21261f637cdf0ff61
Nathanlc <nathanlcastro81@gmail.com>
2025-06-04 22:10:50 -0300

    Extensão mysql2 e conexão com o BD.

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require ('express');
-
 const app = express();
+const mysql = require('mysql2');
 
-app.get("/", function(req, res){
-    res.write("Hello World");
-    res.end();
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'senac',
+    port: 3306,
+    database: 'ecommerce_pc'
+});
+
+conexao.connect(function(erro) {
+    if (erro) {
+        console.error('Erro ao conectar ao banco de dados:',erro);
+        return;
+    }
+    console.log('Conexão com o banco de dados estabelecida com sucesso!');
 });
 
 app.listen(8080);
