595f1bd097d4a5629db169850b429389230cd4fe
Nathanlc <nathanlcastro81@gmail.com>
2025-06-05 20:12:06 -0300

    Instalação e config. do handlebars

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require ('express');
 const app = express();
+const {engine} = require ('express-handlebars');
+
 const mysql = require('mysql2');
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 const conexao = mysql.createConnection({
     host: 'localhost',
@@ -17,6 +23,11 @@ conexao.connect(function(erro) {
     }
     console.log('Conexão com o banco de dados estabelecida com sucesso!');
 });
+
+app.get('/', (req,res)=>{;
+    res.render('teste');
+}
+);
 
 app.listen(8080);
 
